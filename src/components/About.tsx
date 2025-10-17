c9d9487993176559ecb459e7af671fe8c18dd11f
azhemin <azhemin@gmail.com>
2025-10-18 01:27:27 +0300

    Päivitä About: käytä BASE_URL-kuvapolkua

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,11 +9,11 @@ export function About() {
       <div className="container mx-auto px-4">
         <div className="grid md:grid-cols-2 gap-12 items-center">
           <div className="rounded-lg overflow-hidden">
-            <ImageWithFallback
-              src={photo}
-              alt="Anu Kiiruna"
-              className="w-full h-auto"
-            />
+<img
+  src={photo}
+  alt="Anu Kiiruna"
+  className="w-full h-auto"
+/>
           </div>
 
           <div>
